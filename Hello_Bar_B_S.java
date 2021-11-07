c73f1cdfa1cb24bff39004ec66b934980e040130
BarDAP <barbenshabat@gmail.comgit config --global user.name BarDAP>
2021-11-07 22:31:28 +0200

    Still cloudy :(

--- Hello_Bar_B_S.java
+++ Hello_Bar_B_S.java
@@ -1,7 +1,7 @@
 public class Hello_Bar_B_S{
 	public static void main(String[] args) {
 		for (int i=1; i<=5; i++){
-			System.out.println("Sun is almost here");
+			System.out.println("Still cloudy :(");
 		}
 	} 
 }
