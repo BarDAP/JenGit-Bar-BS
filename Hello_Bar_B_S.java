ed634259e8ad3f113fd4472abe2c2aa77599b3aa
BarDAP <barbenshabat@gmail.comgit config --global user.name BarDAP>
2021-11-07 22:33:01 +0200

    There is the sun :)

--- Hello_Bar_B_S.java
+++ Hello_Bar_B_S.java
@@ -1,7 +1,7 @@
 public class Hello_Bar_B_S{
 	public static void main(String[] args) {
 		for (int i=1; i<=5; i++){
-			System.out.println("Still cloudy :(");
+			System.out.println("There is the sun :)");
 		}
 	} 
 }
